2a7d1ee1a752338dec6840d46653a865d286962e
mehmetmerthan <mehmetmerthann@gmail.com>
2023-11-18 17:35:01 +0300

    some changes added

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
 	/*--/ Star Typed /--*/
 	if ($('.text-slider').length == 1) {
-		var typed_strings = $('.text-slider-items').text();
+		var typed_strings = $('.my-slider').text();
 		var typed = new Typed('.text-slider', {
 			strings: typed_strings.split(','),
 			typeSpeed: 80,
@@ -103,7 +103,7 @@
 	/*--/ Testimonials owl /--*/
 	$('#testimonial-mf').owlCarousel({
 		margin: 20,
-		autoplay: true,
+		autoplay: false,
 		autoplayTimeout: 4000,
 		autoplayHoverPause: true,
 		responsive: {
